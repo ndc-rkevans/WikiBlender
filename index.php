452261bf477c1e0866855a2c45803297943ca4ba
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-03-06 17:48:49 -0600

    remove debug code

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-error_reporting( -1 );
-ini_set( 'display_errors', 1 );
-ini_set("log_errors", 1);
-
-// Output errors to log file
-ini_set("error_log", dirname(__FILE__) . "/php.log");
-
-
 require_once "WikiBlender/WikiBlender.php";
 WikiBlender::execute();
