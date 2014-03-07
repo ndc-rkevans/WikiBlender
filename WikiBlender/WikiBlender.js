35741929b155f74d6115476ea65442a6759dd3c9
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-03-07 11:30:30 -0600

    fix active users, plus add tooltip list of users

--- WikiBlender/WikiBlender.js
+++ WikiBlender/WikiBlender.js
@@ -7,20 +7,56 @@ var WikiBlender = {
 		$.getJSON(
 			WikiBlenderServer + wikiPath  + "/api.php",
 			{
+				// site statistics
 				action : "query",
 				meta : "siteinfo",
 				siprop : "statistics",
+				
+				// active users
+				list : "allusers",
+				auactiveusers : "",
+				aulimit : 500,
+				
 				format : "json"
 			},
 			function (response) {
+				var compareUserActions = function (a,b) {
+					if (a.recenteditcount < b.recenteditcount)
+						return 1;
+					if (a.recenteditcount > b.recenteditcount)
+						return -1;
+					return 0;
+				};
+			
 				var stats = response.query.statistics;
-				$(articlesElement).html(
-					stats.articles + " articles, " + 
-					stats.images + " uploaded files<br />" +
-					stats.views + " views, " +
-					stats.edits + " edits<br />" +
-					stats.activeusers + " active contributors"
-				);
+				var allusers = response.query.allusers;
+				
+				allusers.sort( compareUserActions );
+				var userCounts = []
+				for(var u in allusers) {
+					userCounts.push( "<li>" + allusers[u].name + " : " + allusers[u].recenteditcount + "</li>" );
+				}
+				userCounts = userCounts.join("");
+				userCounts = "<h4>User edits in last 30 days</h4><ol>" + userCounts + "</ol>";
+				console.log(userCounts);
+				
+				$(articlesElement)
+					.attr("title", userCounts)
+					.html(
+						stats.articles + " articles, " + 
+						stats.images + " uploaded files<br />" +
+						stats.views + " views, " +
+						stats.edits + " edits<br />" + 
+						allusers.length + " active contributors"
+					)
+					.tooltip({
+						content : function() { return userCounts; }
+					});
+					// .append(
+						// $("<span title='" + userCounts + "'>" + allusers.length + " active contributors<span>").tooltip({
+							// content : function() { return userCounts; }
+						// })
+					// );
 			}
 		);
 
@@ -32,6 +68,6 @@ var WikiBlender = {
 				return $(this).attr('title');
 			}
 		});	
-	}
+	},
 	
 };
