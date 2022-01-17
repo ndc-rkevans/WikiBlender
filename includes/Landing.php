e8f0250cff0c93562959f545f4d8237d5b1bc96f
Rich Evans <32852052+revansx@users.noreply.github.com>
2022-01-17 17:31:50 -0500

    Update Landing.php

--- includes/Landing.php
+++ includes/Landing.php
@@ -49,7 +49,7 @@
 				</div>";
 		}
   	?>
-	<h1 id="first-heading"><?php echo WikiBlender::get_title(); ?></h1>
+	<h1 id="first-heading"><?php echo WikiBlender::get_title(); ?></h1></hr>
 	<p id="subtitle" style='font-size: 2.1em;'>
 		<a href='https://revansx.net/social/'><span style='font-weight:bold;border:1px solid #e0e0e0;border-radius:18px;background-color:#f0f0f0;padding:6px 22px 6px 22px;'>Social</span></a>
 		&nbsp;
@@ -57,7 +57,7 @@
 		&nbsp;
 	  	<a href='https://revansx.net/learn/'><span style='font-weight:bold;border:1px solid #e0e0e0;border-radius:18px;background-color:#f0f0f0;padding:6px 22px 6px 22px;'>Learing</span></a>
 		&nbsp;
-		<span style='font-weight:bold;border:1px solid #e0e0e0;border-radius:18px;padding:6px 22px 6px 22px;'>./Wikis (see below)</span>
+		<span style='font-weight:bold;border:1px solid #e0e0e0;border-radius:18px;padding:6px 22px 6px 22px;'>Wikis (see below)</span>
     <div id="container">
 
     	<?php echo WikiBlender::getSectionTitle( 'header' ); ?>
